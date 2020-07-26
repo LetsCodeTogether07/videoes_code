cc0bf7ae49d14b942dc931a3fb81698c960e1a85
LetsCodeTogether <54933087+LetsCodeTogether07@users.noreply.github.com>
2020-07-26 18:06:10 +0530

    Add files via upload

---
+++ sorting.cpp
@@ -0,0 +1,30 @@
+// sorting =>  1 2 3 4 5 => 5 4 3 2 1;
+
+//     ASCII code
+
+// A: 65
+// Z: 90
+// a: 97
+// z: 122
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char arr[] = {'B', 'C', 'D', 'a', 'E', 'H', 'G', 'F'};
+
+    for (int j = 0; j < 8; j++)
+    {
+        for (int i = 0; i < 8; i++)
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+    }
+
+    for (int i = 0; i < 8; i++)
+        cout << arr[i] << endl;
+}
